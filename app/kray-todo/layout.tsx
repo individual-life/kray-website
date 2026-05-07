e903d228561f2a1334059e645ae1dc1cb18ebac4
NhutTM <nhutminhtran06@gmail.com>
2026-05-07 23:22:57 +0700

    update design to simple

--- app/kray-todo/layout.tsx
+++ app/kray-todo/layout.tsx
@@ -42,10 +42,10 @@ export default function KrayPlannerLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="bg-(--color-white-grey) w-full h-screen  px-[50px] pb-[20px] pt-[20px] flex flex-col">
-      <Header />
+    <div className="bg-(--color-white-grey) w-full h-screen  px-[50px] pb-[20px]  flex flex-col">
+      {/* <Header /> */}
       <div className="flex flex-1">
-        <NavBar />
+        <NavBar hiddenLogout={true} />
         <div className="flex-1 ml-[30px] mt-[30px]">{children}</div>
       </div>
     </div>
--- components/kray-todo/NavBar.tsx
+++ components/kray-todo/NavBar.tsx
@@ -6,7 +6,11 @@ import {
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-const NavBar = () => {
+interface Props {
+  hiddenLogout?: boolean;
+}
+
+const NavBar = ({ hiddenLogout = false }: Props) => {
   const pathname = usePathname();
   const isActiveLink = (href: string) => {
     if (href === "/kray-todo/" && pathname === "/kray-todo") return true;
@@ -33,23 +37,26 @@ const NavBar = () => {
           );
         })}
       </div>
-      <div className="py-[8px] bg-white rounded-full w-[45px] flex flex-col justify-center items-center gap-[15px]">
-        {krayTodoSideLogoutData.map((item, index) => {
-          const isActive = isActiveLink(item.href);
-          return (
-            <Link
-              key={index}
-              href={item.href}
-              className="flex justify-center items-center text-[14px] font-normal cursor-pointer duration-300 ease-in-out"
-            >
-              <div
-                className={`size-[30px] rounded-full flex justify-center items-center ${isActive ? "bg-black text-white" : "hover:bg-gray-100 text-black"}`}
+      <div
+        className={`py-[8px]  rounded-full w-[45px] flex flex-col justify-center items-center gap-[15px] ${!hiddenLogout ? "bg-white" : ""}`}
+      >
+        {!hiddenLogout &&
+          krayTodoSideLogoutData.map((item, index) => {
+            const isActive = isActiveLink(item.href);
+            return (
+              <Link
+                key={index}
+                href={item.href}
+                className="flex justify-center items-center text-[14px] font-normal cursor-pointer duration-300 ease-in-out"
               >
-                <item.icon size={20} />
-              </div>
-            </Link>
-          );
-        })}
+                <div
+                  className={`size-[30px] rounded-full flex justify-center items-center ${isActive ? "bg-black text-white" : "hover:bg-gray-100 text-black"}`}
+                >
+                  <item.icon size={20} />
+                </div>
+              </Link>
+            );
+          })}
       </div>
     </div>
   );
